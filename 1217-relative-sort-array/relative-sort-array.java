b1757c0d8d63f644bc993491db1e1925d547a709
Devesh Agrawal <deveshagrawa84@gmail.com>
2026-01-12 22:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (24.42%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,34 +1,23 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int num : arr1)
-        {
-            map.put(num, map.getOrDefault(num,0)+1);
-        }
-        int c = 0;
+        int[] nums = new int[1001];
 
-        for(int x : arr2) 
-        {
-            int freq = map.getOrDefault(x, 0);
-            for(int j = 0; j < freq; j++) 
-            {
-                arr1[c++] = x;
-            }
-            map.remove(x);
-        }
-        List<Integer> rest = new ArrayList<>();
-        for(int num : map.keySet())
-        {
-            int k = map.getOrDefault(num, 0);
-            for(int j = 0; j < k; j++)
-            {
-                rest.add(num);
+        for (int n : arr1) nums[n]++;
+
+        int count = 0;
+
+        for (int n : arr2) {
+            while (nums[n]-- > 0) {
+                arr1[count++] = n;
             }
         }
-        Collections.sort(rest);
-        for(int x : rest) {
-            arr1[c++] = x;
+
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i]-- > 0) {
+                arr1[count++] = i;
+            }
         }
+
         return arr1;
     }
 }
