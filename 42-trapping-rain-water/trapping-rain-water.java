d4079236de097b7d305ef55cb3d3248d467f1335
Devesh Agrawal <deveshagrawa84@gmail.com>
2026-01-13 23:21:55 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (59.43%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int trap(int[] height) {
+        int res = 0;
+        int l = 0;
+        int r = height.length-1;
+        int lhb = height[0];
+        int rhb = height[height.length-1];
+        while(l<=r)
+        {
+            if(lhb <= rhb)
+            {
+                if(height[l]>= lhb)
+                {
+                    lhb = height[l];
+                }
+                else
+                {
+                    res += lhb - height[l];
+                }
+                l++;
+            }
+            else
+            {
+                if(height[r]>= rhb)
+                {
+                    rhb = height[r];
+                }
+                else
+                {
+                    res += rhb - height[r];
+                }
+                r--;
+            }
+
+        }
+        return res;
+    }
+}
