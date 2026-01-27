5bfad901e2f3f4f10c270dc7ffe9e4a872313e35
Devesh Agrawal <deveshagrawa84@gmail.com>
2026-01-27 23:12:28 +0530

    Time: 17 ms (79.77%) | Memory: 49.9 MB (24.95%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -4,8 +4,7 @@ class Solution {
         int sum=0;
         for(int i = 0; i<nums.length-1; i+=2)
         {
-            int min = Math.min(nums[i],nums[i+1]);
-            sum += min;
+            sum += nums[i];
         }
         return sum;
     }
