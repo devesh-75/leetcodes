e0a7d087910dfcecfc87d29039fe833d95b7f66c
Devesh Agrawal <deveshagrawa84@gmail.com>
2026-02-03 19:27:20 +0530

    Time: 3 ms (88.08%) | Memory: 46.8 MB (26.24%) - LeetSync

---
+++ 764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.java
@@ -0,0 +1,52 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null)
+        {
+            return res;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        
+        queue.offer(root);
+        while(!queue.isEmpty())
+        {
+            List<Integer>temp = new ArrayList<>();
+            
+            int level = queue.size();
+            for(int i=0; i<level; i++)
+            {
+                Node n = queue.poll();
+                temp.add(n.val);
+                if(n.children != null)
+                {
+                    for(Node child : n.children)
+                    {
+                        queue.offer(child);
+                    }
+                }
+            }
+            res.add(temp);
+
+        }
+        return res;
+    }
+}
