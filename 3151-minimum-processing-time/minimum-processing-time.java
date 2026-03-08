c29ba1c41c43fb37cf3b1af9b9e0b3cdb02d9e7e
Devesh Agrawal <deveshagrawa84@gmail.com>
2026-03-08 21:15:27 +0530

    Time: 76 ms (89.09%) | Memory: 86.4 MB (95.00%) - LeetSync

--- 3151-minimum-processing-time/minimum-processing-time.java
+++ 3151-minimum-processing-time/minimum-processing-time.java
@@ -6,15 +6,8 @@ class Solution {
         int j = 0;
         for(int i=tasks.size()-1; i>=3 && j<processorTime.size(); i=i-4)
         {
-            int p1 = processorTime.get(j) + tasks.get(i);
-            int p2 = processorTime.get(j) + tasks.get(i-1);
-            int p3 = processorTime.get(j) + tasks.get(i-2);
-            int p4 = processorTime.get(j) + tasks.get(i-3);
-            int max1 = Math.max(p1,p2);
-            int max2 = Math.max(p3,p4);
-            int max = Math.max(max1,max2);
-            sum = Math.max(sum, max);
-            j++;       
+            sum = Math.max(sum, processorTime.get(j) + tasks.get(i));
+            j++;
         }
         return sum;
     }
