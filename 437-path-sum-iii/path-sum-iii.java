8e56d5add7518da99c2065f7c7bf1c8ebc791f72
Devesh Agrawal <deveshagrawa84@gmail.com>
2026-03-28 22:14:21 +0530

    Time: 20 ms (12.18%) | Memory: 44.8 MB (97.19%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -1,42 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int count = 0;
     public int pathSum(TreeNode root, int targetSum) {
-        
-        if(root == null)
-        {
-            return 0;
-        }
-        func(root,targetSum);
-        pathSum(root.left, targetSum);
-        pathSum(root.right, targetSum);
-        return count;
+        if (root == null) return 0;
+
+        return func(root, targetSum) 
+             + pathSum(root.left, targetSum) 
+             + pathSum(root.right, targetSum);
     }
-    public void func(TreeNode root, long targetSum) {
-        if(root == null)
-        {
-            return;
-        }
-        
-        if(targetSum == root.val)
-        {
-            count++;
-        }
-        func(root.left, targetSum -root.val); 
-        func(root.right, targetSum-root.val);
+
+    public int func(TreeNode root, long targetSum) {
+        if (root == null) return 0;
+
+        int count = 0;
+
+        if (targetSum == root.val) count++;
+
+        count += func(root.left, targetSum - root.val);
+        count += func(root.right, targetSum - root.val);
+
+        return count;
     }
 }
