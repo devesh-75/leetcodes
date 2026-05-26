02de23f2389b87280629bb49000aaf875c8f21aa
Devesh Agrawal <deveshagrawa84@gmail.com>
2026-05-26 11:25:56 +0530

    Time: 21 ms (66.80%) | Memory: 48.3 MB (34.82%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -2,31 +2,21 @@ class Solution {
     public int numberOfSpecialChars(String w) {
         int big[] = new int[26];
         int count = 0;
-        int sm [] = new int[26];
-        for(int i =0; i< 26; i++) 
-        {
-            sm[i] = -1;
-            big[i] = -1;
-        }
-        for(int i=0; i<w.length(); i++)
-        {
+        int sm[] = new int[26];
+        Arrays.fill(big,-1);
+        Arrays.fill(sm,-1);
+        for (int i = 0; i < w.length(); i++) {
             char ch = w.charAt(i);
-            if(Character.isUpperCase(ch))
-            {
-                if(big[ch - 'A'] == -1)
+            if (Character.isUpperCase(ch)) {
+                if (big[ch - 'A'] == -1)
                     big[ch - 'A'] = i;
-            }
-            else
-            {
+            } else {
                 sm[ch - 'a'] = i;
             }
         }
-        for(int i=0; i<26; i++)
-        {
-            if(sm[i] != -1 && big[i] != -1)
-            {
-                if(sm[i] < big[i])
-                {
+        for (int i = 0; i < 26; i++) {
+            if (sm[i] != -1 && big[i] != -1) {
+                if (sm[i] < big[i]) {
                     count++;
                 }
             }
